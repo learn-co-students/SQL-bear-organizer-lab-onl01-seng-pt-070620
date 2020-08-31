3cc654de20f04c2bcaa7fc53fd234ea8f7945e04
Carlos Barreiro <cdrhcr@gmail.com>
2020-08-31 23:01:20 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,72 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+"Mr. Chocolate",
+92,
+"Male",
+"Black",
+"Angry",
+1
+);
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+"Rowdy",
+1,
+"Male",
+"Brown",
+"Happy",
+0
+);
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+"Tabitha",
+23,
+"Female",
+"Red",
+"Bored",
+1
+);
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+"Sergeant Brown",
+56,
+"Male",
+"Orange",
+"Happy",
+1
+);
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+"Melissa",
+27,
+"Female",
+"Orange",
+"Angry",
+0
+);
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+"Grinch",
+82,
+"Male",
+"Black",
+"Confused",
+1
+);
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+"Wendy",
+45,
+"Female",
+"Green",
+"Happy",
+1
+);
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES (
+NULL,
+99,
+"Female",
+"White",
+"Angry",
+0
+); 
