86fc6f595286d657feed7ddff5d4bf4f0cd60b3b
Denise Henderson <denise@henderson3.us>
2020-09-04 19:44:19 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+VALUES
+  ("Mr. Chocolate",5,"Male","Brown","Anxious",1),
+  ("Rowdy",3,"Male","Brown","Hyperactive",1),
+  ("Tabitha",4,"Male","Black","Sanguine",1),
+  ("Sergeant",5,"Male","Brown","Controlled",1),
+  ("Melissa",2,"Male","Brown","Playful",1),
+  ("Grinch",3,"Male","Black","Grumpy",1),
+  ("Wendy",4,"Female","White","Choleric",1);
+INSERT INTO bears (age,gender,color,temperament,alive)
+VALUES
+  (5,"Male","Brown","Angry",1);
