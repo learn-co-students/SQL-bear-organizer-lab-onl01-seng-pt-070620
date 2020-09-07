df05b57e3dafe78160ef633872a63b3976650892
DSJR1284 <DSJR1284@GMAIL.COM>
2020-09-07 06:50:05 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,19 +11,19 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE age = 20 AND name IS NOT NULL"
+  "SELECT name , MAX(age) FROM bears WHERE name IS NOT NULL"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, MIN(age) FROM bears WHERE name IS NOT NULL;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears GROUP BY color"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperaments) FROM bears WHERE temperaments = 'goofy';"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
