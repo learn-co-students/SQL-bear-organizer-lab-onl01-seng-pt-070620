efc2fc25ee66dd3df8b9d9c6c5f8e4b3201fcb86
Srog1989 <s.rogers0715@yahoo.com>
2020-09-08 22:03:17 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, color, gender) VALUES ("Mr. Chocolate", 3, "brown", "M");
+INSERT INTO bears (name, age, color, gender) VALUES ("Rowdy", 5, "black", "M");
+INSERT INTO bears (name, age, color, gender) VALUES ("Tabitha", 1, "brown", "F");
+INSERT INTO bears (name, age, color, gender) VALUES ("Sergeant Brown", 2, "black", "M");
+INSERT INTO bears (name, age, color, gender) VALUES ("Melissa", 2, "black", "F");
+INSERT INTO bears (name, age, color, gender) VALUES ("Grinch", 1, "brown", "M");
+INSERT INTO bears (name, age, color, gender) VALUES ("Wendy", 5, "brown", "F");
+INSERT INTO bears (name, age, color, gender) VALUES (NULL, 3, "black", "F");
+
+
