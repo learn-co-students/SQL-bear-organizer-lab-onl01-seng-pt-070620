46ef24fd96be8b540164a6bf6a5c94163b278ee4
jmd <jordan.michael.dickey@gmail.com>
2020-09-08 22:03:53 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender) VALUES ('Maru', 3, 'male');
+
+INSERT INTO bears (name, age, gender) VALUES ('bob', 4, 'female');
+
+INSERT INTO bears (name, age, gender) VALUES (null, 4, 'male');
+
+INSERT INTO bears (name, age, gender) VALUES ('Jack', 5, 'female');
+
+INSERT INTO bears (name, age, gender) VALUES ('Yogi', 6, 'male');
+
+INSERT INTO bears (name, age, gender) VALUES ('Brown', 7, 'female');
+
+INSERT INTO bears (name, age, gender) VALUES ('Kodiak', 8, 'male');
+
+INSERT INTO bears (name, age, gender) VALUES ('Grizzly', 9, 'female');
